a83971c1bac93eb251b59ceca554dcff7daa11db
Mark-André Hopf <mhopf@mark13.org>
2021-06-28 12:33:25 +0200

    server: update rollup config to avoid warnings

--- rollup-server.config.js
+++ rollup-server.config.js
@@ -2,6 +2,7 @@ import typescript from 'rollup-plugin-typescript2'
 
 export default {
     input: 'src/server/server.ts',
+    external: ['knex', 'crypto', 'bcrypt', 'corba.js', 'corba.js/lib/orb/orb-nodejs'],
     output: {
       file: 'js/server.js',
       name: 'workflow-server',
