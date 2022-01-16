d5f2a4bc84316cd99c71bda0d0922d297aa60239
Mark-André Hopf <mhopf@mark13.org>
2022-01-16 11:09:10 +0100

    fix 'dev:server'

--- index-dev.js
+++ index-dev.js
@@ -1,4 +1,4 @@
-import { main } from "./build/client/workflow.js"
+import { main } from "./build/src/client/workflow.js"
 // main('ws://localhost:8000')"
 // main('ws://'+window.location.hostname+':8000')
 // main('wss://'+window.location.hostname+'/workflow/')
